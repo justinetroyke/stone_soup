c5d0ca50b75958a71cbc14ee86fb124d2083ca22
Justine <jjtroyke@gmail.com>
2018-05-18 20:03:35 -0600

    created first test for manager adds ingredients

---
+++ spec/features/manager_can_add_ingredients_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'manager can add ingredients through /ingredients/new' do
+  scenario 'sees new ingredients form' do
+    visit new_ingredients_path
+
+    expect(page).to have_link('Create Ingredients')
+  end
+end
+
+# expect(page).to have_selector('input', :value =>'item')
+
+# item_1 = 'pork spare ribs'
+# item_2 = 'honey soy marinade'
+# item_3 = 'microwavable white long grain rice'
+# item_4 = 'salad'
+
+# ingredient_1 = Ingredient.create!(item: item_1)
+# ingredient_2 = Ingredient.create!(item: item_2)
+# ingredient_3 = Ingredient.create!(item: item_3)
+# ingredient_4 = Ingredient.create!(item: item_4)
+
+# fill_in 'item', with: item_1
+# fill_in 'item', with: item_2
+# fill_in 'item', with: item_3
+# fill_in 'item', with: item_4
+#
+# expect(current_path).to eq ingredients_path
+# expect(page).to have_content(item_1)
+# expect(page).to have_content(item_2)
+# expect(page).to have_content(item_3)
+# expect(page).to have_content(item_4)
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,10 +55,11 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    # Choose a test framework:
-    with.test_framework :rspec
-    # Or, choose the following (which implies all of the above):
-    with.library :rails
+    config.integrate do |with|
+      # Choose a test framework:
+      with.test_framework :rspec
+      # Or, choose the following (which implies all of the above):
+      with.library :rails
+    end
   end
 end
