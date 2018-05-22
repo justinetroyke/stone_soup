f058594794195caf0160dcc5c3e76d9b4209b9c4
Justine <jjtroyke@gmail.com>
2018-05-22 10:26:34 -0600

    created member test

---
+++ spec/features/members/manager_can_add_members_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+describe 'manager can add members through /members/new' do
+  scenario 'sees new members form' do
+    button = 'Save and Add Another Members'
+    button_2 = 'Create Member'
+
+    visit new_member_path
+
+    expect(page).to have_button(button)
+    expect(page).to have_button(button_2)
+    expect(page).to have_field('member[name]')
+    expect(page).to have_field('member[type]')
+    expect(page).to have_field('member[email]')
+  end
+  #
+  # scenario 'manager adds new member and directs to show' do
+  #   item = 'Onion'
+  #   heading = 'Available Members'
+  #   button_2 = 'Create Member'
+  #   visit new_member_path
+  #
+  #   fill_in 'member[item]', with: item
+  #   click_button button_2
+  #
+  #   expect(current_path).to eq members_path
+  #   expect(page).to have_content item
+  #   expect(page).to have_content heading
+  # end
+  #
+  # scenario 'manager adds new member and clicks add more' do
+  #   item = 'Onion'
+  #   item_2 = 'Honey soy marinade'
+  #   button = 'Save and Add More Members'
+  #   button_2 = 'Create Member'
+  #   visit new_member_path
+  #
+  #   fill_in 'member[item]', with: item
+  #   click_button button
+  #
+  #   expect(current_path).to eq new_member_path
+  #   expect(page).to have_content "#{item} added!"
+  #
+  #   fill_in 'member[item]', with: item_2
+  #   click_button button_2
+  #
+  #   expect(current_path).to eq members_path
+  #   expect(page).to have_content "#{item_2} added!"
+  #   expect(page).to have_content item
+  #   expect(page).to have_content item_2
+  # end
+  #
+  # scenario 'manager gets error message when no item is entered' do
+  #   error_message = "Really!? there is only one field to complete... try again!"
+  #   button = 'Save and Add Another Members'
+  #   button_2 = 'Create Member'
+  #   visit new_member_path
+  #
+  #   click_button button
+  #
+  #   expect(current_path).to eq new_member_path
+  #   expect(page).to have_content error_message
+  #
+  #   click_button button_2
+  #
+  #   expect(current_path).to eq new_member_path
+  #   expect(page).to have_content error_message
+  # end
+end
