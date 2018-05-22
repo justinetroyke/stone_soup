92cadae1e0ea88eee59ff6d950afc5ae1bf836cf
Justine <jjtroyke@gmail.com>
2018-05-22 11:16:45 -0600

    alternate new paths working

--- spec/features/members/manager_can_add_members_spec.rb
+++ spec/features/members/manager_can_add_members_spec.rb
@@ -33,29 +33,38 @@ describe 'manager can add members through /members/new' do
     expect(page).to have_content name
     expect(page).to have_content heading
   end
-  #
-  # scenario 'manager adds new member and clicks add more' do
-  #   item = 'Onion'
-  #   item_2 = 'Honey soy marinade'
-  #   button = 'Save and Add More Members'
-  #   button_2 = 'Create Member'
-  #   visit new_member_path
-  #
-  #   fill_in 'member[item]', with: item
-  #   click_button button
-  #
-  #   expect(current_path).to eq new_member_path
-  #   expect(page).to have_content "#{item} added!"
-  #
-  #   fill_in 'member[item]', with: item_2
-  #   click_button button_2
-  #
-  #   expect(current_path).to eq members_path
-  #   expect(page).to have_content "#{item_2} added!"
-  #   expect(page).to have_content item
-  #   expect(page).to have_content item_2
-  # end
-  #
+
+  scenario 'manager adds new member and clicks add more' do
+    name = 'Justine Troyke'
+    type = 'Member'
+    email = 'bringit@poop.com'
+    name_2 = 'Justin Tro'
+    type_2 = 'Member'
+    email_2 = 'ald@poop.com'
+    button = 'Save and Add Another Member'
+    button_2 = 'Create Member'
+
+    visit new_member_path
+
+    fill_in 'member[name]', with: name
+    fill_in 'member[type]', with: type
+    fill_in 'member[email]', with: email
+    click_button button
+
+    expect(current_path).to eq new_member_path
+    expect(page).to have_content "#{name} added!"
+
+    fill_in 'member[name]', with: name_2
+    fill_in 'member[type]', with: type_2
+    fill_in 'member[email]', with: email_2
+    click_button button_2
+
+    expect(current_path).to eq members_path
+    expect(page).to have_content "#{name_2} added!"
+    expect(page).to have_content name
+    expect(page).to have_content name_2
+  end
+
   # scenario 'manager gets error message when no item is entered' do
   #   error_message = "Really!? there is only one field to complete... try again!"
   #   button = 'Save and Add Another Members'
